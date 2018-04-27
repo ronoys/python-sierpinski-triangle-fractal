6e430db6dcd683a16c4341b639145cb3a3e144f2
ronoys <38772534+ronoys@users.noreply.github.com>
2018-04-26 21:41:25 -0400

    SerpinskiTriangle
    This program prompts the users for the size of the triangle and the number of iterations of the procedure.
    
    The Serpinski triangle image used as a model was found at: https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Sierpinski_triangle.svg/220px-Sierpinski_triangle.svg.png

---
+++ triangleSource.py
@@ -0,0 +1,30 @@
+import turtle
+window = turtle.Screen()
+q = turtle.Turtle()
+size = int(input("Enter the size of the triangle in pixels ")
+level = int(input("Enter the number of levels of the fractal to be created ")
+
+def triangle(x,y):
+
+    # if 
+    if y != 0:
+        triangle(x/2,y-1)
+        q.forward(x/2)
+        triangle(x/2,y-1)
+        q.backward(x/2)
+        q.left(60)
+        q.forward(x/2)
+        q.right(60)
+        triangle(x/2,y-1)
+        q.left(60)
+        q.backward(x/2)
+        q.right(60)
+        
+    else:
+        h = 0
+        while x <= 3:
+            q.forward(x)
+            q.left(120)
+            x = x + 1
+            
+triangle(size,level)
