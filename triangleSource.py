e45b5048cf1f4f81675547a2f36b36e8ba4ab559
ronoys <38772534+ronoys@users.noreply.github.com>
2018-05-01 19:56:04 -0400

    Update triangleSource.py

--- triangleSource.py
+++ triangleSource.py
@@ -1,11 +1,22 @@
-import turtle # imports module
+import turtle
+import random
 window = turtle.Screen()
 q = turtle.Turtle()
 size = int(input("Enter the size of the triangle in pixels "))
 loop = int(input("Enter the number of levels of the fractal to be created "))
 
-def triangle(x,y):
 
+q.penup()
+q.goto(0-(size/2),0-(size/2))
+q.pendown()
+
+
+def triangle(x,y):
+ 
+    colors = ['red','orange','yellow','green', 'blue', 'purple', 'pink']
+    q.fillcolor(random.choice(colors))
+    q.begin_fill()
+    
     if y != 0:
         triangle(x/2,y-1)
         q.forward(x/2)
@@ -24,6 +35,8 @@ def triangle(x,y):
         for i in range(0,3):
             q.forward(x)
             q.left(120)
-  
+            
+    q.end_fill()
             
 triangle(size,loop)
+
